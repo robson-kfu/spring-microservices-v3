b012de04ca2b2506b220cdabdde4d130d0885a07
Robson Silva dos Santos <robson.silva@synchro.com.br>
2024-07-08 15:47:09 -0300

    melhorias de codigo

--- 03.microservices/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ 03.microservices/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,41 +1,44 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
-	
-	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
-	
-	@Autowired
-	private CurrencyExchangeRepository repository;
-	
-	@Autowired
-	private Environment environment;
-	
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
-	public CurrencyExchange retrieveExchangeValue(@PathVariable String from,
-			@PathVariable String to) {
-		
-		logger.info("retrieveExchangeValue called with {} to {}", from, to);
-		
-		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
-		
-		if(currencyExchange ==null) {
-			throw new RuntimeException("Unable to Find data for " + from + " to " + to);
-		}
-		
-		String port = environment.getProperty("local.server.port");
-		currencyExchange.setEnvironment(port);
-		
-		return currencyExchange;
-		
-	}
+
+    private final Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
+    private final CurrencyExchangeRepository repository;
+
+	private final String serverPort;
+
+    public CurrencyExchangeController(CurrencyExchangeRepository repository,
+									  @Value("${server.port}") String pServerPort) {
+        this.repository = repository;
+		this.serverPort = pServerPort;
+    }
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String from,
+                                                  @PathVariable String to) {
+
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
+
+        CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+
+        if (currencyExchange == null) {
+            throw new EntityNotFoundException("Unable to Find currency exchange for " + from + " to " + to);
+        }
+
+        currencyExchange.setEnvironment(serverPort);
+
+        return currencyExchange;
+
+    }
 
 }
---
+++ 03.microservices/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.in28minutes.microservices.currencyexchangeservice;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
