770cb57d5081b1b3e554db2f46482a563bac6962
Robson Silva dos Santos <robson.silva@synchro.com.br>
2024-07-11 14:20:39 -0300

    Testando o CircuitBreakerController

--- 03.microservices/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ 03.microservices/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,5 +1,6 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
+import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -14,23 +15,24 @@ import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 @RestController
 public class CircuitBreakerController {
 	
-	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+	private final Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 	
 	@GetMapping("/sample-api")
-	//@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
-	//@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
-	//@RateLimiter(name="default")
-	@Bulkhead(name="sample-api")
-	//10s => 10000 calls to the sample api
+//	@Retry(name = "default")
+//	@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+//	@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+//	@RateLimiter(name="default")
+	@Bulkhead(name="default")
 	public String sampleApi() {
 		logger.info("Sample api call received");
-//		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", 
+
+//		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
 //					String.class);
 //		return forEntity.getBody();
-		return "sample-api";
+		return "Sample API";
 	}
 	
 	public String hardcodedResponse(Exception ex) {
-		return "fallback-response";
+		return "Error: " + ex.getMessage();
 	}
 }
